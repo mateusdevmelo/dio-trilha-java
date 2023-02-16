8b54dbf384e78452f567d742a4f8813054d36f68
Mateus Melo <mateusdevmelo@gmail.com>
2023-02-16 01:47:11 -0300

    projeto completo

---
+++ conta-banco/src/ContaTest.java
@@ -0,0 +1,27 @@
+public class ContaTest {
+
+    public static void main(String[] args) {
+		ContaTerminal p1 = new ContaTerminal();
+		p1.setNumConta(1111);
+		p1.setDono("Jubileu");
+		p1.abrirConta("CC");
+		
+		
+		ContaTerminal p2 = new ContaTerminal();
+		p2.setNumConta(2222);
+		p2.setDono("Creuza");
+		p2.abrirConta("CP");
+		
+		p1.depositar(100);
+		p2.depositar(500);
+		p2.sacar(100);
+		p1.sacar(150);
+		p1.fecharConta();
+		
+		p1.estadoAtual();
+		p2.estadoAtual();
+	}
+
+}
+    
+
